23f1f1a367876dc15acf019fe61b41f8c2f56d88
rchiarino <rz.chiarino@gmail.com>
2024-09-03 17:34:37 -0300

    added deploy and fixed styles

--- src/app/layout.config.ts
+++ src/app/layout.config.ts
@@ -1,12 +1,12 @@
 import { type DocsLayoutProps } from "fumadocs-ui/layout";
 import { pageTree } from "@/src/app/source";
-import { HomeLayoutProps } from "fumadocs-ui/home-layout";
-// import { version } from "../../package.json";
 
 export const layoutOptions: DocsLayoutProps = {
   tree: pageTree,
   nav: {
     title: "CI/CD - Demo",
+    transparentMode: "top",
+    enableSearch: false,
   },
   sidebar: {
     collapsible: false,
